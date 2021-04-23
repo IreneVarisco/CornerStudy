83e892391e06ec32912885af631a2eb3fbcc8ed2
IreneVarisco <variscoirebne@gmail.com>
2021-04-23 10:02:39 +0200

    graphics

--- bottoni.js
+++ bottoni.js
@@ -1,12 +1,11 @@
 $("#reg").click(function () {
-    var psw = document.getElementById('password').value;                        //controllare se corretto
-    var username = document.getElementById('username').value;
-    window.open("registrazione.php?password=" + psw + "&username=" + username);
-
+  var psw = document.getElementById("password").value;
+  var username = document.getElementById("username").value;
+  window.open("registrazione.php?password=" + psw + "&username=" + username);
 });
 
 $("#log").click(function () {
-    var psw = document.getElementById('password').value;                        //controllare se corretto
-    var username = document.getElementById('username').value;
-    window.open("login.php?password=" + psw + "&username=" + username);
+  var psw = document.getElementById("password").value;
+  var username = document.getElementById("username").value;
+  window.open("login.php?password=" + psw + "&username=" + username);
 });
