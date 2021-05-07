a3206250e957be6ec4778fc6038a568399a70e2c
Lucky Bairagi <luckybairagi96@gmail.com>
2021-05-07 16:38:37 +0530

    Create login.php
    Login file

---
+++ pages/login.php
@@ -0,0 +1,28 @@
+<?php
+	require '../assets/config.php';
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if(isset($username) && isset($password)){
+		$sqlForUsrname = "SELECT COUNT(Username) as total FROM User WHERE Username = '$username'"; //Counting Rows with Username
+		$sqlForPass = "SELECT Password FROM User WHERE Username = '$username'";										//Selecting password using username
+
+		$retUsrname =  mysqli_query($conn, $sqlForUsrname);
+		$rowUsrname = mysqli_fetch_assoc($retUsrname);
+
+		$retPass = mysqli_query($conn, $sqlForPass);
+		$rowPass = mysqli_fetch_assoc($retPass);
+
+		if ($rowUsrname['total'] > 0) {
+			if ($rowPass['Password'] == $password) {
+				echo "<script>alert('Successfull')</script>";
+			}
+		}
+		else{
+			echo "User doesn't exist";
+		}
+
+
+	}
+?>
